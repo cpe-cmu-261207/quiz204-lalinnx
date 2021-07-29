eb8def27589d6710cbefa75ae45b614b2928361b
lalinthorn <lalinpye@gmail.com>
2021-07-29 14:19:27 +0700

    finish

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
+let x ="A"
+const search = document.querySelector("#search")
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  author.innerHTML = 630610758-length.value
-  btn_toggle.innerHTML = "Display Author"
-  
+  if(x=="A"){
+    calculation.innerHTML = 630610758-length.value
+    author.innerHTML = " "
+    btn_toggle.innerHTML = "Display Author"
+    x = "B"
+  }
+  else {
+    author.innerHTML = "630610758 LALINTHORN PHOLNARUK"
+    btn_toggle.innerHTML = "Display Calculation"
+    calculation.innerHTML = " "
+  x = "A"
+  }
 }
 
 // more codes for Search and Reset buttons here
+
+search.onclick = () => {
+
+}
